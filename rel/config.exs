525fff836fb216112702415dee5951a35b368b90
Steve Wagner <ciroque@live.com>
2019-01-18 08:58:32 -0800

    ;;sigh;;

--- rel/config.exs
+++ rel/config.exs
@@ -14,7 +14,7 @@ environment :dev do
 end
 
 environment :prod do
-  set include_erts: false
+  set include_erts: true
   set include_src: false
   set cookie: :"vxJoGJ$eNCZ~YHgvq!8.y$!y;9[YdPK(t?/`5bL~|p1%T_`J]|xgjAzCt1g`1xTD"
   set vm_args: "rel/vm.args"
