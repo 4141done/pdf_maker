064569efed6dde7a86e77e959d470247699c7ade
Steve Wagner <swagner@avvo.com>
2019-01-25 12:48:37 -0800

    Write this the right way

--- lib/pdf_maker_web/controllers/api/html_to_pdf_controller.ex
+++ lib/pdf_maker_web/controllers/api/html_to_pdf_controller.ex
@@ -6,8 +6,9 @@ defmodule PdfMakerWeb.HtmlToPdfController do
 
   def create(conn, %{"html" => html}) do
     conn
-    |> put_resp_content_type("application/pdf")
+    |> put_resp_content_type("application/octet-stream", nil)
+    |> put_resp_header("content-disposition", ~s[attachment; filename="receipt.pdf"])
     |> put_status(:created)
-    |> text(PdfMaker.MakePdf.make_pdf(html))
+    |> send_file(200, PdfMaker.MakePdf.make_pdf(html))
   end
 end
