c08047d5a13ec8178b7c0e13f63aaa02998f5d92
Steve Wagner <swagner@avvo.com>
2019-01-25 07:14:16 -0800

    Adds action_fallback and supporting cast of characters

--- lib/pdf_maker_web/views/error_view.ex
+++ lib/pdf_maker_web/views/error_view.ex
@@ -1,6 +1,38 @@
 defmodule PdfMakerWeb.ErrorView do
   use PdfMakerWeb, :view
 
+  def render("400.json", %{message: message}) do
+    %{error: message}
+  end
+
+  def render("422.json", %{message: message}) do
+    %{error: message}
+  end
+
+  def render("404.json", _assigns) do
+    %{
+      errors: [
+        %{
+          status: 404,
+          title: "Not Found",
+          detail: "Not Found"
+        }
+      ]
+    }
+  end
+
+  def render("500.json", _assigns) do
+    %{
+      errors: [
+        %{
+          status: 500,
+          title: "Internal server error",
+          detail: "Something bad happened"
+        }
+      ]
+    }
+  end
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.html", _assigns) do
